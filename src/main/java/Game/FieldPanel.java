75bceabf1b082f0b3d8abdf619f503d3026c5002
DaimoniX <dimaprotsap@gmail.com>
2022-07-08 23:14:32 +0300

    Reformated resources, added new test map

--- src/main/java/Game/FieldPanel.java
+++ src/main/java/Game/FieldPanel.java
@@ -1,21 +1,25 @@
 package Game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class FieldPanel extends JPanel {
     private final Map map;
+    private final Image[] tiles;
 
     public FieldPanel() {
         super();
-        setBackground(Color.YELLOW);
+        tiles = new Image[14];
+        setBackground(Color.BLACK);
         try {
-            map = Map.fromMapFolder("test_map");
+            map = Map.fromMapFolder("test_map_1");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        loadTiles();
     }
 
     @Override
@@ -25,6 +29,16 @@ public class FieldPanel extends JPanel {
         paintMap(g, map);
     }
 
+    private void loadTiles() {
+        for(int i = 0; i < tiles.length; i++) {
+            try {
+                tiles[i] = ImageIO.read(Objects.requireNonNull(FieldPanel.class.getResource(String.format("/tiles/tile_%d.png", i))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void paintMap(Graphics g, Map map) {
         int width = getWidth();
         int height = getHeight();
@@ -32,13 +46,8 @@ public class FieldPanel extends JPanel {
         int tileHeight = height / map.getHeight();
         for(int y = 0; y < map.getHeight(); y++) {
             for(int x = 0; x < map.getWidth(); x++) {
-                Color c = switch (map.getTile(x, y)) {
-                    case 0 -> Color.YELLOW;
-                    case 1 -> Color.CYAN;
-                    default -> Color.BLACK;
-                };
-                g.setColor(c);
-                g.fillRect(tileWidth * x, tileHeight * y, tileWidth, tileHeight);
+                Image tile = tiles[map.getTile(x, y)];
+                g.drawImage(tile, tileWidth * x, tileHeight * y, tileWidth, tileHeight, null);
             }
         }
     }
