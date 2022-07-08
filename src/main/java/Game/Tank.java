04b9cc48e0b115f3bf76d4f2bafdb7c327fd31c5
Andrii <andriy.perekupka@gmail.com>
2022-07-08 19:30:12 +0300

    added direction and speed for tank

--- src/main/java/Game/Tank.java
+++ src/main/java/Game/Tank.java
@@ -2,9 +2,13 @@ package Game;
 
 public abstract class Tank {
     private int hp;
+    private final int speed;
+    private byte direction;
 
-    protected Tank(int hp) {
+    protected Tank(int hp, int speed, byte direction) {
         this.hp = hp;
+        this.speed =speed;
+        this.direction =direction;
     }
 
     public int getHp() {
@@ -13,5 +17,19 @@ public abstract class Tank {
 
     public void setHp(int hp) {
         this.hp = hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public byte getDirection() {
+        return direction;
+    }
+
+    protected void setDirection(byte direction) {
+        if(direction >3 || direction < 0)
+            throw new IllegalArgumentException();
+        this.direction = direction;
     }
 }
