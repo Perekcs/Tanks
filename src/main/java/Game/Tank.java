afc97708b7a813b69d81729fddc7d5b640eff1e7
Andrii <andriy.perekupka@gmail.com>
2022-07-09 15:42:02 +0300

    added switch case for tank movement

--- src/main/java/Game/Tank.java
+++ src/main/java/Game/Tank.java
@@ -1,9 +1,14 @@
 package Game;
 
-public abstract class Tank {
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Tank  {
     private int hp;
     private final int speed;
     private byte direction;
+    private SimpleVector2 position;
+
 
     protected Tank(int hp, int speed, byte direction) {
         this.hp = hp;
@@ -28,8 +33,16 @@ public abstract class Tank {
     }
 
     protected void setDirection(byte direction) {
-        if(direction >3 || direction < 0)
+        if(direction > 3 || direction < 0)
             throw new IllegalArgumentException();
         this.direction = direction;
+    }
+    public void move (){
+        switch (direction) {
+            case 0 -> position.move(0, 1);
+            case 1 -> position.move(1, 0);
+            case 2 -> position.move(0, -1);
+            case 3 -> position.move(-1, 0);
+        }
     }
 }
